f466248e15e390d1e0fd4ecf01e7a33b5f870ee4
liqiang <1715991019@qq.com>
2024-04-18 14:43:36 +0800

    配置less

--- src/pages/Child/index.tsx
+++ src/pages/Child/index.tsx
@@ -1,5 +1,5 @@
 import React, {FC} from 'react';
-// import styles from './index.module.scss';
+import styles from './index.module.scss';
 
 interface Props {
     classname?: React.CSSProperties | undefined
@@ -8,7 +8,7 @@ interface Props {
 const Child: FC<Props> = ({classname}) => {
   return (
     <div>
-        <div style={classname}>这是一个盒子</div>
+        <div className={styles.box} style={classname}>这是一个盒子</div>
     </div>
   )
 }
